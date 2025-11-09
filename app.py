58ebb7c5a67140264ce06cc092a082676b761582
krishpatel143x-netizen <krishpatel143x@gmail.com>
2025-11-10 01:29:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ st.set_page_config(
 # Initialize Groq client
 @st.cache_resource
 #def get_groq_client():
-    """Initialize Groq client with API key #from secrets"""
+    #"""Initialize Groq client with API key #from secrets"""
     #try:
         api_key = st.secrets["GROQ_API_KEY"]
         return Groq(api_key=api_key)
